295900693a2889352536a3912e3020ea25d087e0
Rahul Kumar <160120718+11222687@users.noreply.github.com>
2024-09-30 19:03:49 +0000

    container config done

---
+++ 01_java_Basic/Main.java
@@ -0,0 +1,18 @@
+
+//public is the keywords and access modifiers
+// main is the class name
+
+public class Main{
+
+    //static is the keywords
+    // void is the variable
+    //main is the fuction or entry gate of java
+
+    public static void main(String[] args) {
+
+        //System is the class
+        //out is the object
+        //println is the method ya fuction
+        System.out.println("hello world");
+    }
+}
